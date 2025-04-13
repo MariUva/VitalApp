25177c597718dc2c93a2f75f7ade8cad69839cd6
Anderson <andersonn.penac@uqvirtual.edu.co>
2025-04-12 22:36:36 -0500

    Correccion test

--- tests/test_app.py
+++ tests/test_app.py
@@ -17,11 +17,13 @@ def client():
 def test_index_route(client):
     response = client.get('/')
     assert response.status_code == 200
-    html = response.data.decode('utf-8')
-    
-    assert "<h1>VitalApp</h1>" in html
-    assert "Bienvenido a tu clínica en línea" in html
-    assert "Agendar Cita" in html
-    assert "Examen de sangre" in html
-    assert "Rayos X" in html
-    assert "Salud Vital" in html
+
+    html = response.data.decode('utf-8').lower()  # Lo pasamos a minúsculas para facilitar las comparaciones
+
+    # Verificamos que el contenido esperado esté presente en el HTML
+    assert "vitalapp" in html
+    assert "bienvenido a tu clínica en línea" in html
+    assert "agendar cita" in html
+    assert "examen de sangre" in html
+    assert "rayos x" in html
+    assert "salud vital" in html
